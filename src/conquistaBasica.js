2d5dd3d97e42f9f6cd64302979b77f08b68e1284
ktabriga <jhonatancatabriga@gmail.com>
2017-06-21 18:05:22 -0300

    Finaliza modo de conquistas basicas; Faz ganho de especialidades;

---
+++ src/conquistaBasica.js
@@ -0,0 +1,50 @@
+import R from 'ramda'
+
+export const verificacao = (target, payload) => (oldValue, newValue) => {
+  if (oldValue < target && newValue >= target) {
+    return {
+      type: 'REACHED',
+      payload
+    }
+  }
+
+  if(oldValue === target && newValue < target) {
+    return {
+      type: 'LOST',
+      payload
+    }
+  }
+
+  return {
+    type: 'NONE'
+  }
+}
+
+const verificacoesDinamicas = options =>
+  R.keys(options)
+    .reduce((verificador, key) =>
+      R.merge(verificador, {
+        [key]: verificacao(options[key].payload)
+      }), {})
+
+
+export default function create(options) {
+  return {
+    introdutorio: verificacao(options.introdutorio.payload),
+    progressao: verificacoesDinamicas(options.progressao),
+    insigniaEspecial: verificacoesDinamicas(options.insigniaEspecial)
+  }
+}
+
+export const nivelEspecialidade = (count, total) =>
+  Math.trunc(count / total / (1/3))
+
+
+export const verificacaoEspecialidade = (oldValue, newValue) =>
+  [
+    verificacao(1/3, {segmento: 'ESPECIALIDADE', nivel: 1})(oldValue, newValue),
+    verificacao(2/3, {segmento: 'ESPECIALIDADE', nivel: 2})(oldValue, newValue),
+    verificacao(1, {segmento: 'ESPECIALIDADE', nivel: 3})(oldValue, newValue)
+  ]
+    .filter(result => result.type !== 'NONE')[0]
+    || {type: 'NONE'}
---
+++ tests/conquistaBasica.js
@@ -0,0 +1,124 @@
+import test from 'ava'
+import create, {verificacao, verificacaoEspecialidade, nivelEspecialidade} from '../src/conquistaBasica'
+
+const senior = {
+  introdutorio: {
+    target: 1,
+    payload: {}
+  },
+  progressao: {
+    escalada: {
+      target: 1/3,
+      payload: {}
+    },
+    conquista: {
+      target: 2/3,
+      payload: {}
+    },
+    azimute: {
+      target: 1,
+      payload: {}
+    }
+  },
+  insigniaEspecial: {
+    imma: {
+      target: 1,
+      payload: {}
+    }
+  }
+}
+
+test('Criar verificador de conquista com metodo por propriedade', t => {
+  const verificador = create(senior)
+  t.truthy(verificador.introdutorio)
+  t.true(typeof verificador.insigniaEspecial.imma === 'function')
+  t.true(typeof verificador.progressao.escalada === 'function')
+  t.true(typeof verificador.progressao.conquista === 'function')
+})
+
+test('Fazer verificação de ganho', t => {
+  const result = verificacao(10, {ramo: 'SENIOR'})(9, 10)
+  t.deepEqual(result, {
+    type: 'REACHED',
+    payload: {ramo: 'SENIOR'}
+  })
+})
+
+test('Fazer verificação de perca', t => {
+  const result = verificacao(10, {ramo: 'SENIOR'})(10, 9)
+  t.deepEqual(result, {
+    type: 'LOST',
+    payload: {ramo: 'SENIOR'}
+  })
+})
+
+test('Faz verificação que não tem mudança', t => {
+  const result = verificacao(10, {})(7,8)
+  t.deepEqual(result, {
+    type: 'NONE'
+  })
+})
+
+test('Calcula nivel da especialidade', t=> {
+  t.is(nivelEspecialidade(3, 9), 1)
+  t.is(nivelEspecialidade(4, 9), 1)
+  t.is(nivelEspecialidade(6, 9), 2)
+  t.is(nivelEspecialidade(8, 9), 2)
+  t.is(nivelEspecialidade(9, 9), 3)
+})
+
+test('Faz verificação de ganho ao passar target', t => {
+  const result = verificacao(1/3, {})(0.32, 0.34)
+  t.deepEqual(result, {
+    type: 'REACHED',
+    payload: {}
+  })
+})
+
+test('Ganho de especialidade nivel 1', t => {
+  const old = 2/9
+  const newValue = 3/9
+  const result = verificacaoEspecialidade(old, newValue)
+  t.deepEqual(result, {
+    type: 'REACHED',
+    payload: {
+      segmento: 'ESPECIALIDADE',
+      nivel: 1
+    }
+  })
+})
+
+test('Não ganha nível especialidade', t => {
+  const old = 3/9
+  const newValue = 4/9
+  const result = verificacaoEspecialidade(old, newValue)
+  t.deepEqual(result, {
+    type: 'NONE'
+  })
+})
+
+test('Ganho de especialidade nivel 2', t => {
+  const old = 5/9
+  const newValue = 6/9
+  const result = verificacaoEspecialidade(old, newValue)
+  t.deepEqual(result, {
+    type: 'REACHED',
+    payload: {
+      segmento: 'ESPECIALIDADE',
+      nivel: 2
+    }
+  })
+})
+
+test('Ganho de especialidade nivel 3', t => {
+  const old = 8/9
+  const newValue = 1
+  const result = verificacaoEspecialidade(old, newValue)
+  t.deepEqual(result, {
+    type: 'REACHED',
+    payload: {
+      segmento: 'ESPECIALIDADE',
+      nivel: 3
+    }
+  })
+})
