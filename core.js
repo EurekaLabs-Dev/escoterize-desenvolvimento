21620ce5bdd12ec76ec904c0d9fd182221a86781
ktabriga <jhonatancatabriga@gmail.com>
2017-07-05 16:45:31 -0300

    Add funções ao façade do desenvolvimento

--- core.js
+++ core.js
@@ -1,10 +1,12 @@
-const desenvolvimento = require('./lib/desenvolvimento.js').default
+const desenvolvimento = require('./lib/desenvolvimento.js')
 const requisito = require('./lib/requisito.js').default
 const especialidade = require('./lib/especialidade.js').default
 const verificacao = require('./lib/verificacao.js').default
 
 const core = {
-  desenvolvimento,
+  desenvolvimento: desenvolvimento.default,
+  percentual: desenvolvimento.percentual,
+  divisoes: desenvolvimento.divisoes,
   requisito,
   especialidade,
   verificacao
